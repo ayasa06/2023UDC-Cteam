abf966c3315ee8c82fbe8ab0540f3ec93932ad76
ayasa06 <111953500+ayasa06@users.noreply.github.com>
2023-11-02 16:48:32 +0900

    環境構築

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cteam.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
