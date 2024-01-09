99d0a39d431ecf62d90985143f0cf5accef9d710
ayasa06 <111953500+ayasa06@users.noreply.github.com>
2024-01-09 13:24:38 +0900

    ErrorViewModel削除
    上書きしてしまったため

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Cteam.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
